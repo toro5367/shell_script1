06508d9ba904bd184c1b7810bff2073d5197e88d
Toro <toro5367@gmail.com>
2023-04-11 15:04:34 -0500

    change to pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-#Description: My first Bash Shell script.
+#Description: Script to install some packaages.
 #Author: Toro 
 #Date: Cctober 2022
+
+if [ ${USER} !=root]
+then 
+echo "you need root access to run this"
+exit 1
+fi 
+yum install finger -y
+yum install curl -y
+yum install zip -y
+yum install vim -y
