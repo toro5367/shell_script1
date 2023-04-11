4dcbb2c2180db0311ae14d23c53440e295f83ebf
Toro <toro5367@gmail.com>
2023-04-11 14:24:11 -0500

    new branch

---
+++ pkg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Description: My first Bash Shell script.
+#Author: Toro 
+#Date: Cctober 2022
---
+++ variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Description: Variables
+#Author: Toro 
+#Date: Cctober 2022
+
+NAME=Toro
+echo "$NAME is a hard working person"
+echo "The manager says he will hire $NAME in his new team"
+echo "$NAME shows dedication in all she does, which makes her stand out "
+echo "$NAME is reliable and always available when needed"
